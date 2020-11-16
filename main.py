033f57134b2185259ca8ce29d268b4b4325e5899
Marvelman3284 <jedimaster3284@gmail.com>
2020-11-16 11:30:43 -0500

    Add colors and clean up code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Imports
 import random
+from colorama import Fore, Style
 
 #Constants
 STATIONARY = "<-->" 
@@ -8,19 +9,19 @@ RIGHT = "-->"
 
 #Variables
 netForce = 0 
-aiChoice = int(random.randint(1, 50))
 
 #Welcome message
-print("Welcome to a tug of war simulator! You are on the right side of a box, and your opponent is on the left side of a box. \nYou will be given a choice of how many newtons of force you would like to apply to the box, and your opponent will be given a choice.\n")
+print(Fore.RED + "Welcome to a tug of war simulator! You are on the right side of a box, and your opponent is on the left side of a box. \nYou will be given a choice of how many newtons of force you would like to apply to the box, and your opponent will be given a choice." + Style.RESET_ALL)
 
 
 while True:
-    userChoice = int((input("How many newtons would you like to apply(Enter a number 1-50)? ")))
+    aiChoice = int(random.randint(1, 50))
+    userChoice = int(input(Fore.BLUE + "\nHow many newtons would you like to apply(Enter a number 1-50)? " + Style.RESET_ALL))
     if userChoice > 50:
         print("Your number must be below 50.")
     #Output
     else:
-        print(f"You applied {userChoice} newtons, your opponent applies {aiChoice} newtons.")
+        print(Fore.GREEN + f"\nYou applied {userChoice} newtons, your opponent applies {aiChoice} newtons.")
 
         if userChoice > aiChoice:
             netForce = userChoice - aiChoice
